a08bd1d3f7f9dd71231768bae7c9299276d144a8
YeonginKim <fhtmah12@gmail.com>
2022-11-05 13:41:12 +0900

    Feat: WebSecurityConfig

---
+++ src/main/java/com/study/security/WebSecurityConfigure.java
@@ -0,0 +1,8 @@
+package com.study.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+}
