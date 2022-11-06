b8b48e00859fcd4cc39d167ffa486dadbff8c92d
YeonginKim <fhtmah12@gmail.com>
2022-11-06 15:33:57 +0900

    WebsecurityConfig edited

--- src/main/java/com/study/security/WebSecurityConfigure.java
+++ src/main/java/com/study/security/WebSecurityConfigure.java
@@ -1,8 +1,38 @@
 package com.study.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+
+  @Override
+  public void configure(WebSecurity web) {
+    web.ignoring().antMatchers("/swagger-resources", "/webjars/**", "/static/**", "/templates/**", "/h2/**");
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .csrf()
+        .disable()
+        .headers()
+        .disable()
+        .exceptionHandling()
+        .and()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .authorizeRequests()
+        .antMatchers("/api/_hcheck").permitAll()
+        .antMatchers("/api/auth").permitAll()
+        .anyRequest().permitAll()
+        .and()
+        // JWT 인증을 사용하므로 form 로긴은 비활성처리
+        .formLogin()
+        .disable();
+  }
 }
