ac6acfebbc6f9e83b566987ce10a39f6f4110327
YeonginKim <fhtmah12@gmail.com>
2022-11-09 22:04:52 +0900

    antmatcher authorizeRequest 수정

--- src/main/java/com/study/security/WebSecurityConfigure.java
+++ src/main/java/com/study/security/WebSecurityConfigure.java
@@ -27,8 +27,6 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
         .authorizeRequests()
-        .antMatchers("/api/_hcheck").permitAll()
-        .antMatchers("/api/auth").permitAll()
         .anyRequest().permitAll()
         .and()
         // JWT 인증을 사용하므로 form 로긴은 비활성처리
