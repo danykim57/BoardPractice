a252028b00f99b3f66df51fba80bbb924554619d
YeonginKim <fhtmah12@gmail.com>
2022-12-07 16:33:21 +0900

    Log changed to log for the export error

--- src/main/java/com/study/filter/TransactionFilter.java
+++ src/main/java/com/study/filter/TransactionFilter.java
@@ -1,6 +1,7 @@
 package com.study.filter;
 
 import jdk.internal.org.jline.utils.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,7 @@ import java.io.IOException;
 
 @Component
 @Order(1)
+@Slf4j
 public class TransactionFilter implements Filter {
 
   @Override
@@ -19,11 +21,11 @@ public class TransactionFilter implements Filter {
       FilterChain chain) throws IOException, ServletException {
 
     HttpServletRequest req = (HttpServletRequest) request;
-        Log.info("Starting a transaction for req : {}",
+        log.info("Starting a transaction for req : {}",
         req.getRequestURI());
 
         chain.doFilter(request, response);
-        Log.info(
+        log.info(
         "Committing a transaction for req : {}",
         req.getRequestURI());
   }
