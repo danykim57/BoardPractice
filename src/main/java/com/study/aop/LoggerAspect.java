fb56edf9d1a2fdda98ff030c5ed482f8cfe04643
YeonginKim <fhtmah12@gmail.com>
2022-12-15 22:09:39 +0900

    HttpServletRequest

--- src/main/java/com/study/aop/LoggerAspect.java
+++ src/main/java/com/study/aop/LoggerAspect.java
@@ -6,19 +6,24 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
 @Slf4j
 @Aspect
 @Component
 public class LoggerAspect {
 
     @Around("execution(* com.study.domain..*Controller.*(..)) || execution(* com.study.domain..*Service.*(..)) || execution(* com.study.domain..*Mapper.*(..))")
-    public Object printLog(ProceedingJoinPoint joinPoint) throws Throwable {
+    public Object printLog(ProceedingJoinPoint joinPoint, HttpServletRequest request) throws Throwable {
 
         String name = joinPoint.getSignature().getDeclaringTypeName();
         String type = "";
 
         if (name.contains("Controller")) {
             type = "Controller ===> ";
+            String uuid = UUID.randomUUID().toString();
+            request.setAttribute("uuid", uuid);
 
         } else if (name.contains("Service")) {
             type = "ServiceImpl ===> ";
