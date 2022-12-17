aab43ab154bd0a3d5f084d462bd9e064c98f252a
YeonginKim <fhtmah12@gmail.com>
2022-12-17 16:32:54 +0900

    Edit unnecessary parts

--- src/main/java/com/study/socket/StompHandler.java
+++ src/main/java/com/study/socket/StompHandler.java
@@ -2,6 +2,7 @@ package com.study.socket;
 
 import com.study.security.Jwt;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
@@ -9,16 +10,14 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
 
-@RequiredArgsConstructor
 @Component
 public class StompHandler implements ChannelInterceptor {
-
-  private final Jwt jwt;
 
   @Override
   public Message<?> preSend(Message<?> message, MessageChannel channel) {
     StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
     if(accessor.getCommand() == StompCommand.CONNECT) {
+      Jwt jwt = new Jwt("BoardTest", "clientSecret", 20);
       Jwt.Claims claims = jwt.verify(accessor.getFirstNativeHeader("Authorization"));
       long userKey = claims.userKey();
       System.out.println("UserKey: " + userKey + "!!");
