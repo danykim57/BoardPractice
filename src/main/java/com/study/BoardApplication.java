fe2e0aec29e0fbf2c8fc520c8dafe6c452f00f0d
YeonginKim <fhtmah12@gmail.com>
2022-12-17 16:12:35 +0900

    Eureka server properties

--- src/main/java/com/study/BoardApplication.java
+++ src/main/java/com/study/BoardApplication.java
@@ -2,8 +2,10 @@ package com.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class BoardApplication {
 
     public static void main(String[] args) {
