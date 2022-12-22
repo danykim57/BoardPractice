caccb874b547ad84539051ffa1bb62140ef9c688
YeonginKim <fhtmah12@gmail.com>
2022-12-22 22:00:22 +0900

    jquery select append

--- src/main/resources/static/scripts/tag.js
+++ src/main/resources/static/scripts/tag.js
@@ -1,36 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
 
-
-    HTML SCSS JS
-
-    Result
-    Skip Results Iframe
-
-EDIT ON
-
-// This demo is using "dragsort" lib (by myself)
-// https://github.com/yairEO/dragsort
-
-
-// The DOM element you wish to replace with Tagify
-var input = document.querySelector('input[name=tags]');
-
-// initialize Tagify on the above input node reference
-var tagify = new Tagify(input)
-
-// bind "DragSort" to Tagify's main element and tell
-// it that all the items with the below "selector" are "draggable"
-var dragsort = new DragSort(tagify.DOM.scope, {
-    selector: '.'+tagify.settings.classNames.tag,
-    callbacks: {
-        dragEnd: onDragEnd
-    }
-})
-
-// must update Tagify's value according to the re-ordered nodes in the DOM
-function onDragEnd(elm){
-    tagify.updateValueByDOMTags()
-}
-
-// listen to tagify "change" event and print updated value
-tagify.on('change', e => console.log(e.detail.value))
-
+ // Handler when the DOM is fully loaded
+  $("#selectBox").append('<option value="option5">option5</option>');
+  $("#selectBox").append('<option value="option7" selected>option7</option>');
+});
